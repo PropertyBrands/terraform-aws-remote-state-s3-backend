4789a877364a46d93908ef5946a78d73583f18f5
pawel osial <pawel.osial@inhabit.com>
2025-04-24 21:17:36 +0200

    changes to ddb with flag to not deploy it by default
    test policy separation to locals
    
    removed unused finally force delete dynamodb var meant to be added in lifecycle for ddb
    
    added fixes to missing filters in bucket lifecycle config + added lifecycle force delete for ddb
    
    removed lifecycle for ddb

--- policy.tf
+++ policy.tf
@@ -7,55 +7,68 @@
 # https://github.com/nozaq/terraform-aws-remote-state-s3-backend/issues/74
 #---------------------------------------------------------------------------------------------------
 
+################################################################################
+# Locals – assemble the policy in parts
+################################################################################
+locals {
+  # statements that are always needed
+  iam_policy_base = [
+    {
+      Effect   = "Allow"
+      Action   = ["s3:ListBucket", "s3:GetBucketVersioning"]
+      Resource = aws_s3_bucket.state.arn
+    },
+    {
+      Effect   = "Allow"
+      Action   = ["s3:GetObject", "s3:PutObject"]
+      Resource = "${aws_s3_bucket.state.arn}/*"
+    },
+    {
+      Effect   = "Allow"
+      Action   = ["kms:ListKeys"]
+      Resource = "*"
+    },
+    {
+      Effect = "Allow"
+      Action = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:DescribeKey",
+        "kms:GenerateDataKey"
+      ]
+      Resource = aws_kms_key.this.arn
+    }
+  ]
+
+  # only present when the lock table is created
+  iam_policy_dynamodb = var.enable_dynamodb_lock ? [
+    {
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:DescribeTable"
+      ]
+      Resource = aws_dynamodb_table.lock[0].arn
+    }
+  ] : []
+}
+
+################################################################################
+# IAM Policy
+################################################################################
 resource "aws_iam_policy" "terraform" {
   count = var.terraform_iam_policy_create ? 1 : 0
 
   name_prefix = var.override_terraform_iam_policy_name ? null : var.terraform_iam_policy_name_prefix
   name        = var.override_terraform_iam_policy_name ? var.terraform_iam_policy_name : null
-  policy      = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket", "s3:GetBucketVersioning"],
-      "Resource": "${aws_s3_bucket.state.arn}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject"],
-      "Resource": "${aws_s3_bucket.state.arn}/*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:DescribeTable"
-      ],
-      "Resource": "${aws_dynamodb_table.lock.arn}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "kms:ListKeys"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:DescribeKey",
-        "kms:GenerateDataKey"
-      ],
-      "Resource": "${aws_kms_key.this.arn}"
-    }
-  ]
-}
-POLICY
+
+  # Build the final document as valid JSON
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = concat(local.iam_policy_base, local.iam_policy_dynamodb)
+  })
 
   tags = var.tags
 }
